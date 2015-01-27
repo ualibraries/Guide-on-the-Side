2dce20151078a148873ca71163dbeb22eae3e4f1
Will Simpson <wsimpson@email.arizona.edu>
2015-01-27 09:40:26 -0700

    Change gots_modal_image_box to be a class rather than an id.

--- app/webroot/js/tutorials/modal_image.js
+++ app/webroot/js/tutorials/modal_image.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-	parent.$('body').append('<div id="gots_modal_image_box"></div>');
+	parent.$('body').append('<div class="gots_modal_image_box"></div>');
 	$('.gots_thumbnail_link').click(function(e){
 		e.preventDefault();
-		parent.$('#gots_modal_image_box').empty();
+		parent.$('.gots_modal_image_box').empty();
 		modal_image = '<img src="' + $(this).attr('href') + '">';
-		parent.$('#gots_modal_image_box').append(modal_image);
-		parent.$('#gots_modal_image_box').dialog({
+		parent.$('.gots_modal_image_box').append(modal_image);
+		parent.$('.gots_modal_image_box').dialog({
 				modal: true,
 				resizeable: false,
 				draggable: false,
