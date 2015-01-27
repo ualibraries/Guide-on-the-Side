c051ccc70e4fa7ac9a08af97f22458205b4cfaf7
Will Simpson <wsimpson@email.arizona.edu>
2015-01-27 11:26:06 -0700

    Fix bug causing modal image box to be off-center.

--- app/webroot/js/tutorials/modal_image.js
+++ app/webroot/js/tutorials/modal_image.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
 	parent.$('body').append('<div class="gots_modal_image_box"></div>');
-	$('.gots_thumbnail_link').click(function(e){
-		e.preventDefault();
-		parent.$('.gots_modal_image_box').empty();
+	$('.gots_thumbnail_link').each(function(){
 		modal_image = '<img src="' + $(this).attr('href') + '">';
 		parent.$('.gots_modal_image_box').append(modal_image);
+	});
+	$('.gots_thumbnail_link').click(function(e){
+		parent.$('.gots_modal_image').removeClass('gots_modal_image');
+		parent.$('img[src="'+ $(this).attr('href')+'"]').addClass('gots_modal_image');
+		e.preventDefault();
 		parent.$('.gots_modal_image_box').dialog({
 				modal: true,
 				resizeable: false,
