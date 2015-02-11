4462b72f39b7e0e88c1d3c8fee622b12faf93b29
Will Simpson <wsimpson@email.arizona.edu>
2015-02-11 11:52:01 -0700

    Make popup image windows match the image aspect ratio. Ref #75

--- app/webroot/js/tutorials/popup_image.js
+++ app/webroot/js/tutorials/popup_image.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
 	var viewportHeight = $(window).height();
 	$('.gots_thumbnail_link').click(function(e){
+		aspectRatio = $(this).children('img').attr('width')/$(this).children('img').attr('height');
+		height = 1.1*viewportHeight;
+		width = height * aspectRatio;
 		e.preventDefault();
-		options = "height=" + viewportHeight + ", width=" + viewportHeight;
+		options = "height=" + height + ", width=" + width;
 		window.open($(this).attr('href'), '', options);
 	});
 });
