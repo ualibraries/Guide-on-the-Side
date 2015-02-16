3aa4f61c242863d5619c4a74fb2cdebbf4aa98ed
Will Simpson <wsimpson@email.arizona.edu>
2015-02-16 11:47:30 -0700

    Fix popup image issue for very small/long images.  Ref #75

--- app/webroot/js/tutorials/popup_image.js
+++ app/webroot/js/tutorials/popup_image.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
 	var viewportHeight = $(window).height();
+	$('body').append('<div class="gots_modal_image_box"></div>');
+	$('.gots_thumbnail_link').each(function(){
+		popup_image = '<img src="' + $(this).attr('href') + '">';
+		$('.gots_modal_image_box').append(popup_image);
+	});
 	$('.gots_thumbnail_link').click(function(e){
-		aspectRatio = $(this).children('img').attr('width')/$(this).children('img').attr('height');
-		height = 1.1*viewportHeight;
-		width = height * aspectRatio;
+		width = $('img[src="'+ $(this).attr('href')+'"]').width();
+		height = $('img[src="'+ $(this).attr('href')+'"]').height();
+		aspectRatio = width/height;
+		if(height > 600 || width > (600*aspectRatio)){
+			height = 600;
+			width = height * aspectRatio;
+		}
 		e.preventDefault();
 		options = "height=" + height + ", width=" + width;
 		window.open($(this).attr('href'), '', options);
