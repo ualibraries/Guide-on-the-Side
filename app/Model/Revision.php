f7cad449a969c284b3d58d57afcf28ae41a485a2
Will Simpson <wsimpson@email.arizona.edu>
2015-02-17 12:01:39 -0700

    Add validation for required fields for Revision and Role.  Ref #25

--- app/Model/Revision.php
+++ app/Model/Revision.php
@@ -3,5 +3,11 @@ class Revision extends AppModel {
 	var $name = 'Revision';
 
   var $belongsTo = array('Tutorial', 'User');
+	public $validate = array(
+		'operation' => array(
+			'rule' => 'notEmpty',
+			'required' => true
+		)
+	);
 }
 ?>
--- app/Model/Role.php
+++ app/Model/Role.php
@@ -3,6 +3,12 @@
 class Role extends AppModel {
 
     var $hasMany = array('User');
+	public $validate = array(
+		'name' => array(
+			'rule' => 'notEmpty',
+			'required' => true
+		)
+	);
 
 }
 ?>
